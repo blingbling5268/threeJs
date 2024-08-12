caca668fa99c926b0237541e98e301c3828f00a1
liubo <lb@hzguode.com>
2024-08-12 10:39:34 +0800

    fix: 测试代码

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,21 @@
  * @Author: liubo lb@hzguode.com
  * @Date: 2023-08-28 14:37:02
  * @LastEditors: liubo lb@hzguode.com
- * @LastEditTime: 2023-09-04 16:38:16
+ * @LastEditTime: 2024-08-12 10:39:07
  * @FilePath: /guangde-map/src/App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <router-view></router-view>
 </template>
+
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  console.log('=========>初始化', 初始化);
+})
+</script>
 
 
 <style lang="scss">
